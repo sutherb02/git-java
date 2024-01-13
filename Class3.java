8fb4a2d9337d6c6e5b3587fee36d51451715b014
Blake Sutherland <tsutherland1@stu.jsu.edu>
2024-01-13 14:13:39 -0600

    Added Class3

---
+++ Class3.java
@@ -0,0 +1,13 @@
+public class Class3 {
+    private void f1(){
+        // placeholder method v1
+    }
+
+    private void f2(){
+        // placeholder method v2
+    }
+
+    private void f3(){
+        // placeholder method v3
+    }
+}
